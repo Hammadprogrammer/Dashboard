d929dd43630ca49a8c6732713bfa65ef82cc442a
Hammadprogrammer <hammadzahid221@gmail.com>
2025-09-07 14:09:35 +0500

    updated work

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from "../sharecomponent/navbar/navbar"
+import HajjDashboardPage from './dashboard/hajj/page'
+import UmrahDashboardPage from './dashboard/umrah/page'
+import DomesticDashboardPage from './dashboard/domestic/page'
+import InternationalTourDashboard from './dashboard/international-tour/page'
 
 const page = () => {
   return (
     <div>
         <Navbar/>
+        <HajjDashboardPage/>
+        <UmrahDashboardPage/>
+        <DomesticDashboardPage/>
+        <InternationalTourDashboard/>
     </div>
   )
 }
